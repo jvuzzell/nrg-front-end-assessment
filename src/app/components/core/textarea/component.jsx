832e3ae51549afa980e93a845cae68110bc4da6f
Joshua Uzzell <josh.uzzell@gmail.com>
2023-01-19 06:12:31 -0500

    Add support for multiple buttons in textarea component

--- src/app/components/core/textarea/component.jsx
+++ src/app/components/core/textarea/component.jsx
@@ -1,16 +1,24 @@
 import './component.css';
 
-const Headline = (props) => {
+const Headline = (props) => { 
+
+    let buttons = []; 
+
+    props.content.buttons.map((button, index) => { 
+        buttons.push(<a 
+                        className={button.classList} 
+                        href={button.link.destination} 
+                        key={index}
+                    >
+                        {button.displayText}
+                    </a>);
+    });
+
     return (
         <div className="textarea vertical-align">
             <h1 className="light-weight">{props.content.heading}</h1>
-            <p className="has-mg-bottom-20">{props.content.textarea}</p>
-            <a 
-                className={props.content.buttons[0].classList} 
-                href={props.content.buttons[0].link.destination}
-            >
-                {props.content.buttons[0].displayText}
-            </a>
+            <p className="has-mg-bottom-20">{props.content.textarea}</p> 
+            {buttons}
         </div>
     );  
 }
