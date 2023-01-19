9b9f0df173994d398d7f29912e6ba68e49742d5d
Joshua Uzzell <josh.uzzell@gmail.com>
2023-01-19 06:58:28 -0500

    Add support for reversing columns on two column image and textarea component

--- src/app/components/layout/twoColumn_TextareaAndImage/component.jsx
+++ src/app/components/layout/twoColumn_TextareaAndImage/component.jsx
@@ -3,16 +3,22 @@ import Textarea from '../../core/textarea/component';
 import Image from '../../core/image/component';
 import Consumer from '../../../contexts/configContext';
 
-const TwoColumn_headlineAndImage = (props) => { 
+const TwoColumn_headlineAndImage = (props) => {  
+
+
+    
     return (
         <Consumer>
             {ctx => { 
+                let imageColumnClassList = "v-col md-3 lg-4 ";
+                imageColumnClassList += ctx[ props.parentComponent ][ 'twoColumn_TextareaAndImage' ][ 'imageOrientation' ];
+
                 const textareaContent = ctx[ props.parentComponent ][ 'twoColumn_TextareaAndImage' ][ 'textarea' ]; 
                 const imageContent = ctx[ props.parentComponent ][ 'twoColumn_TextareaAndImage' ][ 'image' ]; 
 
                 return(  
                     <div className="twoColumnHeadlineImage h-row clearfix" >
-                        <div className="v-col md-3 lg-4 float-right">
+                        <div className={imageColumnClassList}>
                             <Image content={ imageContent }/>
                         </div>
                         <div className="v-col md-3 lg-4">
