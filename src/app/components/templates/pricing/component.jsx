4e4d7e5357e31747af4eae75e7f4bb3cc197c40a
Joshua Uzzell <josh.uzzell@gmail.com>
2023-01-20 23:14:19 -0500

    Enable dynamic inclusion of card components via cardType property and transitions image import from core image component into template component

--- src/app/components/templates/pricing/component.jsx
+++ src/app/components/templates/pricing/component.jsx
@@ -1,11 +1,12 @@
 import './component.css'; 
 import ThreeColumn_Cards from '../../layout/threeColumn_Cards/component';
+import '../../../assets/icons/fontawesome-6.2.0/light/plug-circle-bolt.svg';
 
 const Pricing = () => {
     return ( 
         <section className="price-tier has-mg-bottom-40">
             <div className="content-lock">
-                <ThreeColumn_Cards parentComponent="pricing"/>
+                <ThreeColumn_Cards parentComponent="pricing" cardType="PriceTierCard" />
             </div>
         </section>
     );  
